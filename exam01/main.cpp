9ac2bff1a204dedec6e0bc3b84b6d5aadb8f1541
ee2405 <rio714tw@gmail.com>
2020-04-01 17:46:54 +0800

    new

---
+++ exam01/main.cpp
@@ -0,0 +1,21 @@
+#include "mbed.h"
+
+PwmOut PWM1(D7);
+
+int main() {
+    double i;
+    PWM1.period(0.01);
+
+    while(1){
+        for(i=0;i<1;i+=0.1){ 
+            PWM1 = i;
+            wait(0.1);
+        }
+        while(i>0){
+            PWM1 = i;
+            wait(0.1);
+            i-=0.1;
+        }
+    }
+
+}
